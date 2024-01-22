8b9a9141ec079614a1bded5e54c1a77e9cb56a35
Sami Salkini <samisalkinii@gmail.com>
2024-01-22 21:12:08 +0300

    HTML Sematic tags

---
+++ src/components/Footer.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <img src="logo192.png" alt="logo" />
+      <ul>
+        Doormat Navigation
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+      <ul>
+        Contact
+        <li>
+          <a href="/">Address</a>
+        </li>
+        <li>
+          <a href="/">phone number</a>
+        </li>
+        <li>
+          <a href="/">email</a>
+        </li>
+      </ul>
+      <ul>
+        Social media links
+        <li>
+          <a href="/">Address</a>
+        </li>
+        <li>
+          <a href="/">Phone number</a>
+        </li>
+        <li>
+          <a href="/">email</a>
+        </li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="logo192.png" alt="Logo" />
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
